88f9ebb3bc417a205c09ce1fe30b0d7726612d0f
eduardom <jemm9920@gmail.com>
2022-11-29 12:15:38 -0600

    Uso de js para login

--- assets/js/formulario-validacion.js
+++ assets/js/formulario-validacion.js
@@ -1,1 +1,14 @@
-holi
+const nombre = document.getElementById("exampleInputName")
+const email = document.getElementById("exampleInputEmail1")
+const pass = document.getElementById("exampleInputPassword1")
+const form = document.getElementById("form1")
+
+form.addEventListener("submit",e=>){
+    e.preventDefault()
+    let warnings=""
+    let entrar = false
+    let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(nombre.ariaValueMax.length<6){
+        warnings += `El nombre no es valido <br>`
+    }
+}

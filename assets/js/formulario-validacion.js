3b74a52f7763b9f4a3189129e66b99b145592c5e
idrodrigo <megj980623hjcdrn05@gmail.com>
2022-12-06 13:33:01 -0600

    validaciondeloginjs

--- assets/js/formulario-validacion.js
+++ assets/js/formulario-validacion.js
@@ -1,19 +1,22 @@
 const nombre = document.getElementById("name")
 const email = document.getElementById("email")
 const pass = document.getElementById("password")
-const form = document.getElementById("form1")
+const passDos = document.getElementById("passDos")
+const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
 
 form.addEventListener("submit",e=>{
     e.preventDefault()
-    let warnings= ""
-    let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    let warnings= "";
+    let entrar = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    parrafo.innerHTML = ""
     if(nombre.value.length<6){
+        
         warnings += `El nombre no es valido <br>`
-        entrar=true
+        entrar=true 
     }
-    if(!regexEmail.test(email.value)){
+if(!regexEmail.test(email.value)){
         warnings += `El email no es valido <br>`
         entrar=true
     }
@@ -21,9 +24,15 @@ form.addEventListener("submit",e=>{
         warnings += `La contraseña no es valida <br>`
         entrar=true
     }
+    if(passDos.value !== pass.value){
+        warnings += `La contraseña no es igual <br>`
+        entrar=true
+    }
     if(entrar){
         parrafo.innerHTML = warnings
-    }
+    }else{
+        parrafo.innerHTML = "Enviado"
+    } 
 
 
 })

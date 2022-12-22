dd34190df937fc36f69f374b95cf545257a68ba9
yael <luya12@gmail.com>
2022-12-22 09:46:50 -0600

    cambios de Ana

--- assets/js/formulario-validacion.js
+++ assets/js/formulario-validacion.js
@@ -1,38 +1,57 @@
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const pass = document.getElementById("password")
-const passDos = document.getElementById("passDos")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
-
-form.addEventListener("submit",e=>{
-    e.preventDefault()
-    let warnings= "";
-    let entrar = false;
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    parrafo.innerHTML = ""
-    if(nombre.value.length<6){
-        
-        warnings += `El nombre no es válido <br>`
-        entrar=true 
-    }
-if(!regexEmail.test(email.value)){
-        warnings += `El email no es válido <br>`
-        entrar=true
-    }
-    if(pass.value.length<8){
-        warnings += `La contraseña no es válida <br>`
-        entrar=true
-    }
-    if(passDos.value !== pass.value){
-        warnings += `La contraseña no es igual <br>`
-        entrar=true
-    }
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }else{
-        parrafo.innerHTML = "Enviado"
-    } 
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+const passDos = document.getElementById("passDos");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
 
 
-})
+
+form.addEventListener("submit", (e) => {
+  let warnings = "";
+  let entrar = false;
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  parrafo.innerHTML = "";
+  if (nombre.value.length < 6) {
+    warnings += `El nombre no es válido <br>`;
+    entrar = true;
+  }
+  if (!regexEmail.test(email.value)) {
+    warnings += `El email no es válido <br>`;
+    entrar = true;
+  }
+  if (pass.value.length < 8) {
+    warnings += `La contraseña no es válida <br>`;
+    entrar = true;
+  }
+  if (passDos.value !== pass.value) {
+    warnings += `La contraseña no es igual <br>`;
+    entrar = true;
+  }
+  if (entrar) {
+    parrafo.innerHTML = warnings;
+  } else {
+    let data={
+        "correo":`${email.value}`,
+        "nombre_completo":`${nombre.value}`,
+        "pass":`${pass.value}`
+    }
+
+    fetch("http://localhost:8080/usuarios", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    console.log(data);
+    parrafo.innerHTML = "Enviado";
+  }
+});
---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+/* let nombreFormulario;
+let emailFormulario;
+let passwordFormulario;
+
+function obtenerDatos(){
+    nombreFormulario=document.getElementById("name").value;
+    emailFormulario=document.getElementById("email").value;
+    passwordFormulario=document.getElementById("password").value;
+
+    console.log(nombreFormulario);
+    console.log(emailFormulario);
+    console.log(passwordFormulario);
+} */
